9b88e9a6e333636e51dfe75271a57e9a59c244c6
Angel <angeljpa95@gmail.com>
2021-02-17 11:34:55 -0600

    Validaciones del form terminadas

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,6 +1,7 @@
 const d = document,
   $btnContact = d.getElementById("btnContact"),
-  $form = d.getElementById("contact"),
+  $formSection = d.getElementById("contact"),
+  $form = d.querySelector(".contact-form"),
   $contactsInfo = d.querySelectorAll(".contact-info"),
   $labels = d.querySelectorAll(".contact-info label"),
   $inputs = d.querySelectorAll(".contact-form [required]");
@@ -19,5 +20,49 @@ $labels.forEach((label) => {
 /* Scroll to Contact Form */
 d.addEventListener("click", (e) => {
   if (!e.target.matches(`#btnContact`)) return;
-  $form.scrollIntoView();
+  $formSection.scrollIntoView();
+});
+
+/* Form validation */
+d.addEventListener("keyup", (e) => {
+  if (e.target.matches(".contact-form [required]")) {
+    let $required = e.target,
+      pattern = $required.pattern || $required.dataset.pattern;
+
+    //console.log($input,pattern);
+
+    if (pattern && $required.value !== "") {
+      //Si tiene patron...
+      let regex = new RegExp(pattern);
+      return !regex.exec($required.value)
+        ? d.getElementById($required.name).classList.add("is-active")
+        : d.getElementById($required.name).classList.remove("is-active");
+    }
+
+    if (!pattern) {
+      //si NO tiene patron...
+      return $required.value === ""
+        ? d.getElementById($required.name).classList.add("is-active")
+        : d.getElementById($required.name).classList.remove("is-active");
+    }
+  }
+});
+
+/* Loader and msg  */
+d.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //alert("Enviando Formulario");
+
+  const $loader = d.querySelector(".contact-form-loader"),
+    $response = d.querySelector(".contact-form-response");
+
+  $loader.classList.remove("none");
+
+  setTimeout(() => {
+    $loader.classList.add("none");
+    $response.classList.remove("none");
+    $form.reset();
+
+    setTimeout(() => $response.classList.add("none"), 2500);
+  }, 2000);
 });
