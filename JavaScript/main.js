21b7c9c6904e20426780b2c0ed590abd4e98fdb2
angelp495 <angeljpa95@gmail.com>
2021-06-14 19:40:34 -0500

    query: tablet version

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,15 +1,34 @@
 const d = document,
-  btnCV = d.getElementById("btn-cv"),
-  btnModalCVs = d.getElementById("btn-modal-cvs");
+  $btnCV = d.getElementById("btn-cv"),
+  $btnModalCVs = d.getElementById("btn-modal-cvs"),
+  $form = d.getElementsByName("form"),
+  $btnToTop = d.getElementById("btn-top");
 
 d.addEventListener("click", (e) => {
-  if (e.target == btnCV) {
-    btnModalCVs.classList.add("active");
-  } else if (e.target == btnModalCVs) {
-    btnModalCVs.classList.remove("active");
+  if (e.target == $btnCV) {
+    $btnModalCVs.classList.add("active");
+  } else if (e.target == $btnModalCVs) {
+    $btnModalCVs.classList.remove("active");
   }
 
   if (e.target.classList.contains("cv-link")) {
-    btnModalCVs.classList.remove("active");
+    $btnModalCVs.classList.remove("active");
+  }
+
+  if (e.target == $btnToTop || e.target.matches("#btn-top *")) {
+    d.body.scrollTop = 0;
+    d.documentElement.scrollTop = 0;
+  }
+});
+
+window.addEventListener("scroll", (e) => {
+  let scroll = window.pageYOffset,
+    pageWidth = window.innerWidth;
+
+  /* Efectos para pantalla movil */
+  if (pageWidth > 320 && scroll > 500) {
+    $btnToTop.classList.add("active");
+  } else {
+    $btnToTop.classList.remove("active");
   }
 });
