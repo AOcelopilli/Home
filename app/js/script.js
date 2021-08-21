7c51757b0be553960e8973d3315402a2798f8b93
angelp495 <angeljpa95@gmail.com>
2021-08-21 14:59:05 -0500

    form

---
+++ app/js/script.js
@@ -0,0 +1,57 @@
+const d = document,
+  $form = d.getElementById("form"),
+  $loader = d.getElementById("loader"),
+  $formMsg = d.getElementById("form-msg");
+
+d.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  $loader.classList.add("active");
+
+  const { name, email, subject, comments } = $form;
+
+  fetch("https://formsubmit.co/ajax/angeljpa95@gmail.com", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name.value,
+      email: email.value,
+      subject: subject.value,
+      comments: comments.value,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.success == "false") {
+        let $msg = d.createElement("h3");
+        $msg.innerText = "Ocurrio un error, por favor vuelve a intentar";
+
+        $formMsg.appendChild($msg);
+
+        setTimeout(() => {
+          $formMsg.removeChild($msg);
+        }, 3000);
+      }
+
+      //TODO: Se envia un arreglo vacio
+      if (data.success) {
+        let $msg = d.createElement("h3");
+        $msg.innerText = "Los datos han sido enviados con exito.";
+
+        $formMsg.appendChild($msg);
+
+        setTimeout(() => {
+          $formMsg.removeChild($msg);
+        }, 3000);
+      }
+    })
+    .catch((err) => {
+      console.log(err.msg);
+    });
+
+  $loader.classList.remove("active");
+  $form.reset();
+});
